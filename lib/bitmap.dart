d76a640c4b5f3110776b287e6cf45f52e78be475
exergysystems1 <exergysystems01@gmail.com>
2026-01-19 16:32:44 +0500

    ## 1.0.1
    * feat: Upgrade to Flutter 3.32.5 compatibility (tested with Dart 3.2.6)
    * feat: Update to Android Gradle Plugin 8.9.1
    * feat: Update to Gradle 8.11.1
    * feat: Update to Kotlin 2.1.0
    * feat: Use FFI 2.1.0 (compatible with Dart 3.2.0+)
    * feat: Increase compile SDK to 35
    * feat: Increase target SDK to 35
    * feat: Increase min SDK from 16 to 24
    * feat: Upgrade Java from 8 to 17
    * feat: Enable Gradle configuration cache
    * feat: Enable parallel builds
    * feat: Enable Gradle build caching
    * feat: Optimize JVM arguments for better performance
    * feat: Enable R8 full mode
    * feat: Enable non-transitive R class
    * docs: Add comprehensive upgrade guide
    
    ## 1.0.0
    
    * Initial stable release
    * Standardize version numbering
    
    ## 0.2.0
    
    * fix: Add support for android gradle version 8.1.3
    * feat: Implement RGB overlay operation
    
    ## 0.1.3
    
    * Bump ffi to 2.0.1
    
    ## 0.1.2
    
    * Fix wrong throw on resize image
    
    ## 0.1.1
    
    * Fix wrong assertion on resize image
    
    ## 0.1.0
    
    * Support FFI 1.0.0
    * Add crop operation
    * Add rotation operation
    * Change API to `bitmap.apply(Operation())`
    * Add batch operations
    * Update plugin to support Flutter 1.20.0
    
    ## 0.0.1
    
    * Initial release

--- lib/bitmap.dart
+++
@@ -1,5 +0,0 @@
-library bitmap;
-
-export 'src/bitmap.dart';
-export 'src/ffi.dart';
-export 'src/operation/operation.dart';
--- test/bitmap_test.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Figure out a way to test this with FFI', () {});
-}
